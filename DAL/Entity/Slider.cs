9a9f9fe8702188d6c8b185f71a97bcb16d093f72
ozanbelkeci <belkeci.ozan@gmail.com>
2024-06-25 21:58:23 +0300

    evcekis

---
+++ DAL/Entity/Slider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Entity
+{
+    public class Slider : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Photo { get; set; }
+    }
+}
