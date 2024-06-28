b60c32a7ee3626969f992b9c5c50a16c25b17039
Anıl Düz <96021036+Anilduz@users.noreply.github.com>
2024-06-28 14:13:35 +0300

    changed page view

---
+++ DAL/Models/PageModel.cs
@@ -0,0 +1,17 @@
+﻿using DAL.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class PageModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public string PageHtml { get; set; }
+    }
+}
---
+++ EShopperAdminPanel/Controllers/PageController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EShopperAdminPanel.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
