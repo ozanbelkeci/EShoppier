567e1b82368af750b0ef18e46ec1b14659974f81
Anıl Düz <96021036+Anilduz@users.noreply.github.com>
2024-06-28 16:35:47 +0300

    landing page creator page design for admin

--- EShopperAdminPanel/Controllers/PageController.cs
+++ EShopperAdminPanel/Controllers/PageController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DAL.Entity;
+using DAL.Operations;
+using EShopperAdminPanel.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EShopperAdminPanel.Controllers
 {
@@ -6,8 +11,24 @@ namespace EShopperAdminPanel.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            GenericRepository<Page> repository = new GenericRepository<Page>();
+
+            var products = repository.GetList();
+
+            List<PageModel> pageModel = products.Select(i => new PageModel()
+            {
+                Name = i.Name,
+                Description = i.Description,
+                PageHtml = i.PageHtml
+            }).ToList();
+            return View(pageModel);
         }
 
+        public IActionResult CreateLandingPage(string viewName, string pageHtml)
+        {
+            var cshtmlcontent = string.Format(@"@{Layout = ""~/Views/Shared/_Layout.cshtml"";ViewData[""{0}""] = ""Page Title"";} {0}",pageHtml);
+            System.IO.File.WriteAllText(@"EShoppier\EShopperMVC\Views\Home\" + viewName + ".cshtml", cshtmlcontent);
+            return Json(new { redirectToUrl = Url.Action(viewName) });
+        }
     }
 }
